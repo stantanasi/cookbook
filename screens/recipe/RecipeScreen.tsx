2b45f4e88e23f37a024ad45d4dec501ae7d99ab3
Lory-Stan TANASI <stantanasi@gmail.com>
2024-04-11 18:16:19 +0200

    feat(recipe): retrieve selected recipe

--- screens/recipe/RecipeScreen.tsx
+++ screens/recipe/RecipeScreen.tsx
@@ -1,8 +1,17 @@
-import { StyleSheet, View } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function RecipeScreen() {
+import { recipes } from '../../data/recipes';
+import { RootStackParamList } from '../../navigation/types';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Recipe'>;
+
+export default function RecipeScreen({ route }: Props) {
+  const recipe = recipes.find((recipe) => recipe.id === route.params.id)
+
   return (
     <View style={styles.container}>
+      <Text>{recipe?.title}</Text>
     </View>
   );
 }
