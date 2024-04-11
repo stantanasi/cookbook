a1e80fb71433933439f2427088b5b699ef130501
Lory-Stan TANASI <stantanasi@gmail.com>
2024-04-11 18:13:26 +0200

    feat: create recipe screen

---
+++ screens/recipe/RecipeScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, View } from 'react-native';
+
+export default function RecipeScreen() {
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
