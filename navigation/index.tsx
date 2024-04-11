ae79816645fdf5cffe09420922941a6bb0133a4b
Lory-Stan TANASI <stantanasi@gmail.com>
2024-04-11 11:53:58 +0200

    feat(app): getting started React Navigation

---
+++ navigation/index.tsx
@@ -0,0 +1,12 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { View, Text } from "react-native";
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    </NavigationContainer>
+  );
+}
