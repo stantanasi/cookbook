e65b88d1100a4bdc5e03fdc8ad600d69086491ea
Lory-Stan TANASI <stantanasi@gmail.com>
2024-04-11 15:39:39 +0200

    feat: create recipe type

---
+++ types/recipe.type.ts
@@ -0,0 +1,23 @@
+export interface IRecipe {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  preparationTime: number;
+  cookingTime: number;
+  ingredients: {
+    title: string;
+    items: {
+      quantity: number;
+      unit: string;
+      name: string;
+      image?: string;
+    }[];
+  }[];
+  steps: {
+    title: string;
+    actions: string[];
+  }[];
+  createdAt: string;
+  updatedAt: string;
+}
