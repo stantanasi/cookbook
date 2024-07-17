2d71885012026a817b2759e5b031ba0b77d7bccf
Lory-Stan TANASI <stantanasi@gmail.com>
2024-07-17 10:48:50 +0200

    feat: create recipe save screen

---
+++ screens/recipe-save/RecipeSaveScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+type Props = {}
+
+export default function RecipeSaveScreen(props: Props) {
+  return (
+    <Text>RecipeSaveScreen</Text>
+  )
+}
