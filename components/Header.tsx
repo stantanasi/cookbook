6b1ad59d89c8d514ea8f9694bff5fdd4a039063f
Lory-Stan TANASI <stantanasi@gmail.com>
2024-08-10 15:47:11 +0200

    feat(header): add search text input

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,20 @@
 import { NativeStackHeaderProps } from '@react-navigation/native-stack'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, TextInput, View } from 'react-native'
 
 type Props = NativeStackHeaderProps
 
 export default function Header({ navigation }: Props) {
   return (
-    <View>
-      <Text>Header</Text>
+    <View style={styles.container}>
+      <TextInput
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
