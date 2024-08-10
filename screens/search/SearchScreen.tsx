a8efa9ac5047e2dd094098f284eab877fb91aeb6
Lory-Stan TANASI <stantanasi@gmail.com>
2024-08-10 14:24:03 +0200

    feat: create search screen

---
+++ screens/search/SearchScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+type Props = {}
+
+export default function SearchScreen({}: Props) {
+  return (
+    <View>
+      <Text>SearchScreen</Text>
+    </View>
+  )
+}
