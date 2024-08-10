cb67bb26224fe9f24ff71841180f024b6207f712
Lory-Stan TANASI <stantanasi@gmail.com>
2024-08-10 15:20:28 +0200

    feat(search): display recipes by query

--- screens/search/SearchScreen.tsx
+++ screens/search/SearchScreen.tsx
@@ -1,17 +1,32 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
 import { RootStackParamList } from '../../navigation/types';
 import RecipeApi from '../../utils/RecipeApi';
+import Recipe from './components/Recipe';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Search'>;
 
-export default function SearchScreen({ route }: Props) {
+export default function SearchScreen({ navigation, route }: Props) {
   const query = route.params.query
   const recipes = RecipeApi.search(query)
 
   return (
     <View style={styles.container}>
+      <FlatList
+        data={recipes}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({ item }) => (
+          <Pressable onPress={() => navigation.navigate('Recipe', { id: item.id })}>
+            <Recipe recipe={item} />
+          </Pressable>
+        )}
+        contentContainerStyle={styles.recipes}
+        ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
+        ListHeaderComponent={() => (
+          <Text>Recette {query}</Text>
+        )}
+      />
     </View>
   )
 }
@@ -19,5 +34,11 @@ export default function SearchScreen({ route }: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  recipes: {
+    paddingStart: 12,
+    paddingTop: 20,
+    paddingEnd: 12,
+    paddingBottom: 20,
   },
 })
