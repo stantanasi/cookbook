29aedf5e4720f17b0df0bef6cf81e54ef128fbae
Lory-Stan TANASI <stantanasi@gmail.com>
2024-08-15 15:41:17 +0200

    feat(login): implement saving GitHub API token

--- screens/login/LoginScreen.tsx
+++ screens/login/LoginScreen.tsx
@@ -1,14 +1,30 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React from 'react'
+import React, { useState } from 'react'
 import { RootStackParamList } from '../../navigation/types';
-import { Text, View } from 'react-native';
+import { Button, Text, TextInput, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Login'>
 
-export default function LoginScreen({ }: Props) {
+export default function LoginScreen({ navigation }: Props) {
+  const [token, setToken] = useState('')
+
   return (
     <View>
       <Text>Login</Text>
+      <TextInput
+        value={token}
+        onChangeText={(value) => setToken(value)}
+        placeholder='GitHub API Token'
+        secureTextEntry
+      />
+      <Button
+        title='Login'
+        onPress={async () => {
+          await AsyncStorage.setItem("github_token", token)
+          navigation.navigate('Home')
+        }}
+      />
     </View>
   )
 }
