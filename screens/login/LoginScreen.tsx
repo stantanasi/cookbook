cae8b9b725bf0de75d1de8424167a8d36a9e2efc
Lory-Stan TANASI <stantanasi@gmail.com>
2024-08-15 15:29:23 +0200

    feat: create login screen

---
+++ screens/login/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+type Props = {}
+
+export default function LoginScreen({ }: Props) {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  )
+}
