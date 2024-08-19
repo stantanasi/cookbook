51ac2e098667fc647b85d774408c58d11da2aa2a
Lory-Stan TANASI <stantanasi@gmail.com>
2024-08-19 20:31:16 +0200

    feat(profile): display user data

--- screens/profile/ProfileScreen.tsx
+++ screens/profile/ProfileScreen.tsx
@@ -1,14 +1,30 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import React from 'react'
+import React, { useContext } from 'react'
 import { Text, View } from 'react-native'
 import { RootStackParamList } from '../../navigation/types'
+import { AuthContext } from '../../contexts/AuthContext'
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Profile'>
 
 export default function ProfileScreen({ }: Props) {
+  const { user, logout } = useContext(AuthContext)
+
+  if (!user) {
+    return <View></View>
+  }
+
   return (
     <View>
-      <Text>Profile</Text>
+      <Text>
+        {user.name ?? user.pseudo}
+      </Text>
+      <Text>
+        @{user.pseudo}
+      </Text>
+
+      <Text>
+        {user.bio}
+      </Text>
     </View>
   )
 }
