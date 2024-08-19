0ca774edcdefa4c140d79ba2a4ceaeede5851430
Lory-Stan TANASI <stantanasi@gmail.com>
2024-08-19 20:12:01 +0200

    feat: create profile screen

---
+++ screens/profile/ProfileScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+type Props = {}
+
+export default function ProfileScreen({ }: Props) {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  )
+}
