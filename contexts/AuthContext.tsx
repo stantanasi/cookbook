78c482b171260cb855850f624df3e0bc0d800496
Lory-Stan TANASI <stantanasi@gmail.com>
2024-08-25 12:08:39 +0200

    perf(auth-context): optimize authentication

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -5,7 +5,6 @@ import Octokit from '../utils/octokit/octokit'
 
 interface IAuthContext {
   isReady: boolean
-  token: string | null
   isAuthenticated: boolean
   user: IUser | null
   login: (token: string) => Promise<void>
@@ -14,7 +13,6 @@ interface IAuthContext {
 
 export const AuthContext = createContext<IAuthContext>({
   isReady: false,
-  token: null,
   isAuthenticated: false,
   user: null,
   login: async () => { },
@@ -23,23 +21,16 @@ export const AuthContext = createContext<IAuthContext>({
 
 export default function AuthProvider({ children }: PropsWithChildren) {
   const [isReady, setIsReady] = useState(false)
-  const [token, setToken] = useState<IAuthContext['token']>(null)
   const [user, setUser] = useState<IAuthContext['user']>(null)
 
   useEffect(() => {
-    AsyncStorage.getItem('github_token')
-      .then((value) => setToken(value))
-      .finally(() => setIsReady(true))
-  }, [])
+    async function prepare() {
+      const token = await AsyncStorage.getItem('github_token')
 
-  useEffect(() => {
-    if (!token) {
-      return
-    }
+      if (token) {
+        const octokit = new Octokit({ auth: token })
+        const user = await octokit.users.getAuthenticatedUser()
 
-    const octokit = new Octokit({ auth: token })
-    octokit.users.getAuthenticatedUser()
-      .then((user) => {
         setUser({
           id: user.id,
           pseudo: user.login,
@@ -52,28 +43,42 @@ export default function AuthProvider({ children }: PropsWithChildren) {
           following: user.following,
           url: user.html_url,
         })
-      })
-  }, [token])
+      }
+    }
+
+    prepare()
+      .catch((err) => console.error(err))
+      .finally(() => setIsReady(true))
+  }, [])
 
   return (
     <AuthContext.Provider
       value={{
         isReady: isReady,
-        token: token,
-        isAuthenticated: !!token,
+        isAuthenticated: !!user,
         user: user,
 
         login: async (token) => {
           const octokit = new Octokit({ auth: token })
-          return octokit.users.getAuthenticatedUser()
-            .then(() => {
-              setToken(token)
-              return AsyncStorage.setItem('github_token', token)
-            })
+          const user = await octokit.users.getAuthenticatedUser()
+
+          setUser({
+            id: user.id,
+            pseudo: user.login,
+            avatar: user.avatar_url,
+            name: user.name,
+            bio: user.bio,
+            location: user.location,
+            company: user.company,
+            followers: user.followers,
+            following: user.following,
+            url: user.html_url,
+          })
+          return AsyncStorage.setItem('github_token', token)
         },
 
         logout: async () => {
-          setToken(null)
+          setUser(null)
           return AsyncStorage.removeItem('github_token')
         },
       }}
