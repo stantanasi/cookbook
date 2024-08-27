37f8ddde29752870656b8e74a39ec1abb5bd68d7
Lory-Stan TANASI <stantanasi@gmail.com>
2024-08-27 14:21:48 +0200

    feat(utils): create function isJson to check if string is JSON

--- utils/utils.ts
+++ utils/utils.ts
@@ -97,3 +97,12 @@ export function round(value: number, precision: number) {
   var multiplier = Math.pow(10, precision || 0);
   return Math.round(value * multiplier) / multiplier;
 }
+
+export function isJson(str: string) {
+  try {
+    JSON.parse(str)
+  } catch (e) {
+    return false
+  }
+  return true
+}
