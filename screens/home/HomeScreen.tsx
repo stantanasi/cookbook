427c3227dbe4ae78344d8be4a4a54f4107107949
Lory-Stan TANASI <stantanasi@gmail.com>
2024-08-29 16:47:44 +0200

    feat(home): hide scrollbar indicator on categories

--- screens/home/HomeScreen.tsx
+++ screens/home/HomeScreen.tsx
@@ -19,6 +19,7 @@ const Header = ({ recipes, categories, selectedCategory, onSelectCategory }: {
     <>
       <ScrollView
         horizontal
+        showsHorizontalScrollIndicator={false}
         contentContainerStyle={{
           gap: 10,
           paddingHorizontal: 16,
