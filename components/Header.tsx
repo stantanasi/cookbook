6d2c0dd4b5522fc808a6a7a7d5758585c6900023
Lory-Stan TANASI <stantanasi@gmail.com>
2024-09-06 15:53:56 +0200

    feat(header): use bottom sheet modal to login

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,160 @@
 import { MaterialIcons } from '@expo/vector-icons';
 import { NativeStackHeaderProps, NativeStackNavigationProp } from '@react-navigation/native-stack';
 import Constants from 'expo-constants';
-import React, { useContext } from 'react';
-import { Image, Pressable, StyleSheet, TextInput, View } from 'react-native';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { ActivityIndicator, Animated, Dimensions, Image, Modal, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 import { AuthContext } from '../contexts/AuthContext';
 import { RootStackParamList } from '../navigation/types';
+
+const LoginModal = ({ visible, onRequestClose }: {
+  visible: boolean
+  onRequestClose: () => void
+}) => {
+  const { login } = useContext(AuthContext)
+  const [token, setToken] = useState('')
+  const [isLogging, setIsLogging] = useState(false)
+  const panY = useRef(new Animated.Value(Dimensions.get('screen').height)).current;
+
+  const top = panY.interpolate({
+    inputRange: [-1, 0, 1],
+    outputRange: [0, 0, 1],
+  })
+
+  useEffect(() => {
+    if (visible) {
+      Animated.timing(panY, {
+        useNativeDriver: false,
+        toValue: 0,
+        duration: 300,
+      }).start();
+    } else {
+      Animated.timing(panY, {
+        useNativeDriver: false,
+        toValue: Dimensions.get('screen').height,
+        duration: 500,
+      }).start()
+    }
+  }, [visible])
+
+  return (
+    <Modal
+      animationType="fade"
+      onRequestClose={() => onRequestClose()}
+      transparent
+      visible={visible}
+    >
+      <Pressable
+        onPress={() => onRequestClose()}
+        style={{
+          backgroundColor: '#00000052',
+          flex: 1,
+          justifyContent: 'flex-end',
+        }}
+      >
+        <Animated.View
+          style={[{
+            backgroundColor: '#fff',
+            borderTopLeftRadius: 10,
+            borderTopRightRadius: 10,
+          }, { top }]}
+        >
+          <Pressable>
+            <View
+              style={{
+                alignItems: 'center',
+                borderBottomColor: '#ddd',
+                borderBottomWidth: 1,
+                flexDirection: 'row',
+                padding: 16,
+              }}
+            >
+              <Text
+                style={{
+                  flex: 1,
+                  fontSize: 16,
+                  fontWeight: 'bold',
+                }}
+              >
+                Connexion
+              </Text>
+              <MaterialIcons
+                name="close"
+                size={24}
+                color="#000"
+                onPress={() => onRequestClose()}
+              />
+            </View>
+
+            <Text
+              style={{
+                fontSize: 26,
+                fontWeight: 'bold',
+                paddingHorizontal: 16,
+                marginTop: 16,
+                textAlign: 'center',
+              }}
+            >
+              Me connecter
+            </Text>
+
+            <TextInput
+              value={token}
+              onChangeText={(value) => setToken(value)}
+              placeholder="GitHub API Token"
+              placeholderTextColor="#a1a1a1"
+              secureTextEntry
+              style={{
+                borderColor: '#EAEDE8',
+                borderRadius: 4,
+                borderWidth: 1,
+                marginTop: 24,
+                marginHorizontal: 20,
+                paddingHorizontal: 6,
+                paddingVertical: 8,
+              }}
+            />
+
+            <View
+              style={{
+                alignItems: 'center',
+                backgroundColor: '#000',
+                borderRadius: 10,
+                flexDirection: 'row',
+                justifyContent: 'center',
+                gap: 12,
+                marginHorizontal: 20,
+                marginVertical: 24,
+                padding: 16,
+              }}
+            >
+              <Text
+                onPress={async () => {
+                  setIsLogging(true)
+
+                  await login(token)
+
+                  setIsLogging(false)
+                  onRequestClose()
+                }}
+                style={{
+                  color: '#fff',
+                  fontWeight: 'bold',
+                }}
+              >
+                Se connecter
+              </Text>
+              <ActivityIndicator
+                animating={isLogging}
+                color="#fff"
+              />
+            </View>
+          </Pressable>
+        </Animated.View>
+      </Pressable>
+    </Modal>
+  )
+}
+
 
 type Props = NativeStackHeaderProps & {
   query: string
@@ -14,6 +164,7 @@ type Props = NativeStackHeaderProps & {
 export default function Header({ query, onChangeQuery, ...props }: Props) {
   const navigation = props.navigation as NativeStackNavigationProp<RootStackParamList>
   const { user } = useContext(AuthContext)
+  const [isLoginModalVisible, setLoginModalVisible] = useState(false)
 
   return (
     <View style={styles.container}>
@@ -48,10 +199,17 @@ export default function Header({ query, onChangeQuery, ...props }: Props) {
           if (user) {
             navigation.navigate('Profile', { id: user.id })
           } else {
-            navigation.navigate('Login')
+            setLoginModalVisible(true)
           }
         }}
       />
+
+      {!user && (
+        <LoginModal
+          visible={isLoginModalVisible}
+          onRequestClose={() => setLoginModalVisible(false)}
+        />
+      )}
     </View>
   )
 }
