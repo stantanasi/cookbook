6cd72d636737034affa6540ea0f5bc88d04f4bbc
Lory-Stan TANASI <stantanasi@gmail.com>
2025-07-31 11:06:31 +0200

    feat(home): retrieve data from Redux

--- screens/home/HomeScreen.tsx
+++ screens/home/HomeScreen.tsx
@@ -1,51 +1,16 @@
 import { StaticScreenProps, useNavigation } from '@react-navigation/native';
-import { useEffect, useState } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 import slugify from 'slugify';
 import RecipeCard from '../../components/molecules/RecipeCard';
-import Category, { CATEGORY_ALL } from '../../models/category.model';
-import Recipe from '../../models/recipe.model';
-import LoadingScreen from '../loading/LoadingScreen';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import { useHome } from './hooks/useHome';
 
 type Props = StaticScreenProps<undefined>
 
-export default function HomeScreen({ }: Props) {
+export default function HomeScreen({ route }: Props) {
   const navigation = useNavigation()
-  const [recipes, setRecipes] = useState<Recipe[]>([])
-  const [categories, setCategories] = useState<Category[]>([])
-  const [selectedCategory, setSelectedCategory] = useState<Category>(CATEGORY_ALL)
-
-  const [isReady, setIsReady] = useState(false)
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', async () => {
-      setIsLoading(true)
-
-      const categories = await Category.find()
-      const recipes = await Recipe.find({
-        ...{ isNew: false },
-        ...(!!selectedCategory.id && { category: selectedCategory.id }),
-      })
-        .sort({ updatedAt: 'descending' })
-
-      setRecipes(recipes)
-      setCategories([
-        CATEGORY_ALL,
-        ...categories,
-      ])
-      setIsReady(true)
-      setIsLoading(false)
-    });
-
-    return unsubscribe;
-  }, [navigation, selectedCategory])
-
-  if (!isReady) {
-    return <LoadingScreen />
-  }
+  const { categories, recipes, selectedCategory, setSelectedCategory } = useHome(route.params)
 
   return (
     <View style={styles.container}>
@@ -63,22 +28,11 @@ export default function HomeScreen({ }: Props) {
         )}
         ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
         ListHeaderComponent={Header({
-          isLoading: isLoading,
           recipes: recipes,
           categories: categories,
           selectedCategory: selectedCategory,
-          onSelectCategory: async (category) => {
-            setIsLoading(true)
-
-            const recipes = await Recipe.find({
-              ...{ isDraft: false },
-              ...(!!category.id && { category: category.id }),
-            })
-              .sort({ updatedAt: 'descending' })
-
-            setRecipes(recipes)
+          onSelectCategory: (category) => {
             setSelectedCategory(category)
-            setIsLoading(false)
           }
         })}
         ListFooterComponent={Footer()}
