81e91566ab3fb0a2448e609d27f05a3d49dda4ba
Lory-Stan TANASI <stantanasi@gmail.com>
2025-07-31 11:09:29 +0200

    feat(search): retrieve data from Redux

--- screens/search/SearchScreen.tsx
+++ screens/search/SearchScreen.tsx
@@ -1,11 +1,9 @@
 import { StaticScreenProps, useNavigation } from '@react-navigation/native';
-import React, { useEffect, useLayoutEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import slugify from 'slugify';
 import RecipeCard from '../../components/molecules/RecipeCard';
-import Recipe, { IRecipe } from '../../models/recipe.model';
-import { FilterQuery } from '../../utils/database';
-import LoadingScreen from '../loading/LoadingScreen';
+import { useSearch } from './hooks/useSearch';
 
 export type SearchFilterQuery = {
   includeIngredients?: string
@@ -21,105 +19,13 @@ type Props = StaticScreenProps<{
 
 export default function SearchScreen({ route }: Props) {
   const navigation = useNavigation()
-  const { query, ...filter } = route.params
-  const [recipes, setRecipes] = useState<Recipe[]>([])
-
-  const [isReady, setIsReady] = useState(false)
-  const [isLoading, setIsLoading] = useState(true)
-
-  const fetchRecipes = async () => {
-    setIsLoading(true)
-
-    const recipes = await Recipe.search(query, {
-      $and: ([] as FilterQuery<IRecipe>[])
-        .concat({
-          $or: filter.category?.split(',')
-            .filter((value) => value)
-            .map((value: any) => {
-              return { category: value }
-            })
-            ?? []
-        })
-        .concat({
-          $or: filter.cuisine?.split(',')
-            .filter((value) => value)
-            .map((value: any) => {
-              return { cuisine: value }
-            })
-            ?? []
-        }),
-    })
-      .then((recipes) => {
-        if (!filter.includeIngredients) return recipes
-
-        const includedIngredients = new Set(filter.includeIngredients.split(','));
-        return recipes.filter((recipe) =>
-          recipe.steps.some((step) =>
-            step.ingredients.some((ingredient) =>
-              includedIngredients.has(ingredient.name)
-            )
-          )
-        )
-      })
-      .then((recipes) => {
-        if (!filter.excludeIngredients) return recipes
-
-        const excludeIngredients = new Set(filter.excludeIngredients.split(','));
-        return recipes.filter((recipe) =>
-          !recipe.steps.some((step) =>
-            step.ingredients.some((ingredient) =>
-              excludeIngredients.has(ingredient.name)
-            )
-          )
-        )
-      })
-      .then((recipes) => {
-        if (!filter.totalTime) return recipes
-
-        return recipes.filter((recipe) => {
-          const recipeTotalTime = recipe.preparationTime + recipe.cookingTime + recipe.restTime
-
-          return filter.totalTime?.split(',')
-            .some((filterTotalTime) => {
-              if (filterTotalTime === '30') {
-                return recipeTotalTime <= 30
-              } else if (filterTotalTime === '30-60') {
-                return recipeTotalTime >= 30 && recipeTotalTime <= 60
-              } else if (filterTotalTime === '60') {
-                return recipeTotalTime >= 60
-              } else {
-                return false
-              }
-            })
-        })
-      })
-
-    setRecipes(recipes)
-    setIsReady(true)
-    setIsLoading(false)
-  }
+  const { recipes } = useSearch(route.params)
 
   useLayoutEffect(() => {
     navigation.setOptions({
       title: `Recettes ${route.params.query}`,
     })
-  }, [navigation])
-
-  useEffect(() => {
-    fetchRecipes()
-  }, [route.params])
-
-  useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
-      fetchRecipes()
-    })
-
-    return unsubscribe
   }, [navigation, route.params])
-
-  if (!isReady) {
-    return <LoadingScreen />
-  }
 
   return (
     <View style={styles.container}>
@@ -138,23 +44,12 @@ export default function SearchScreen({ route }: Props) {
         ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
         ListHeaderComponent={() => (<>
           <Text style={styles.title}>
-            Recette {query}
+            Recette {route.params.query}
           </Text>
-          {isLoading && (
-            <ActivityIndicator
-              animating={isLoading}
-              color="#000"
-              style={{
-                alignSelf: 'center',
-                marginBottom: 20,
-                marginTop: 10,
-              }}
-            />
-          )}
         </>)}
         ListEmptyComponent={() => (
           <Text style={styles.emptyListText}>
-            Pas de résultats pour {query}
+            Pas de résultats pour {route.params.query}
           </Text>
         )}
         ListFooterComponent={() => <View style={{ height: 20 }} />}
---
+++ screens/search/hooks/useSearch.ts
@@ -0,0 +1,72 @@
+import { ComponentProps } from "react";
+import Recipe from "../../../models/recipe.model";
+import { useAppSelector } from "../../../redux/store";
+import SearchScreen from "../SearchScreen";
+
+export const useSearch = (params: ComponentProps<typeof SearchScreen>['route']['params']) => {
+  const recipes = useAppSelector((state) => {
+    let result = Recipe.searchRedux(state, params.query, {
+      filter: {
+        $and: [
+          {
+            $or: params.category?.split(',')
+              .filter((value) => value)
+              .map((value) => ({ category: value }))
+              ?? [],
+          },
+          {
+            $or: params.cuisine?.split(',')
+              .filter((value) => value)
+              .map((value) => ({ cuisine: value }))
+              ?? [],
+          },
+        ],
+      },
+    });
+
+    if (params.includeIngredients) {
+      const includedIngredients = new Set(params.includeIngredients.split(','));
+      result = result.filter((recipe) =>
+        recipe.steps.some((step) =>
+          step.ingredients.some((ingredient) =>
+            includedIngredients.has(ingredient.name)
+          )
+        )
+      );
+    }
+
+    if (params.excludeIngredients) {
+      const excludeIngredients = new Set(params.excludeIngredients.split(','));
+      result = result.filter((recipe) =>
+        !recipe.steps.some((step) =>
+          step.ingredients.some((ingredient) =>
+            excludeIngredients.has(ingredient.name)
+          )
+        )
+      );
+    }
+
+    if (params.totalTime) {
+      result = result.filter((recipe) => {
+        const recipeTotalTime = recipe.preparationTime + recipe.cookingTime + recipe.restTime;
+
+        return params.totalTime?.split(',')
+          .some((filterTotalTime) => {
+            if (filterTotalTime === '30') {
+              return recipeTotalTime <= 30;
+            } else if (filterTotalTime === '30-60') {
+              return recipeTotalTime >= 30 && recipeTotalTime <= 60;
+            } else if (filterTotalTime === '60') {
+              return recipeTotalTime >= 60;
+            } else {
+              return false;
+            }
+          });
+      });
+    }
+
+    return result;
+  });
+
+  return { recipes };
+}
