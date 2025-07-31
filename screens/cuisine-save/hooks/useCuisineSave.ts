3ec6da8c7dd56cdf3f27af131ee10f4af6f915e4
Lory-Stan TANASI <stantanasi@gmail.com>
2025-07-31 12:36:39 +0200

    feat(cuisine-save): implement retrieve/save cuisine with Redux

---
+++ screens/cuisine-save/hooks/useCuisineSave.ts
@@ -0,0 +1,26 @@
+import { ComponentProps, useEffect, useMemo, useState } from "react";
+import Cuisine, { ICuisine } from "../../../models/cuisine.model";
+import { useAppSelector } from "../../../redux/store";
+import CuisineSaveScreen from "../CuisineSaveScreen";
+
+export const useCuisineSave = (params: ComponentProps<typeof CuisineSaveScreen>['route']['params']) => {
+  const [form, setForm] = useState<ICuisine>(undefined as any);
+
+  const cuisine = (() => {
+    if (!params) {
+      return useMemo(() => new Cuisine({
+      }), [params]);
+    }
+
+    return useAppSelector((state) => {
+      return Cuisine.findByIdRedux(state, params.id);
+    });
+  })();
+
+  useEffect(() => {
+    if (!cuisine || form) return
+    setForm(cuisine.toObject());
+  }, [cuisine]);
+
+  return { cuisine, form, setForm };
+}
